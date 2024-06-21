6a237f808ee5fe37c3cddd93c6c3bb03a7c2d1b8
NyllRE <nyll.redeast@gmail.com>
2024-06-22 02:30:09 +0300

    completed the base documentation

--- app.config.ts
+++ app.config.ts
@@ -1,53 +1,59 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
-  docus: {
-    title: 'Nuxt File Storage',
-    description: 'Easy solution to store files in your nuxt apps',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+	docus: {
+		mdc: {
+			highlight: {
+				langs: ['sql'],
+			},
+		},
+		title: 'Nuxt File Storage',
+		description: 'Easy solution to store files in your nuxt apps',
+		image:
+			'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
 
-    socials: {
-      twitter: 'nyllre',
-      github: 'nyllre/nuxt-file-storage',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
-    },
+		socials: {
+			twitter: 'nyllre',
+			github: 'nyllre/nuxt-file-storage',
+			nuxt: {
+				label: 'Nuxt',
+				icon: 'simple-icons:nuxtdotjs',
+				href: 'https://nuxt.com/modules/nuxt-file-storage',
+			},
+		},
 
-    github: {
-      dir: '.starters/default/content',
-      branch: 'master',
-      repo: 'nuxt-file-storage-docs',
-      owner: 'nyllre',
-      edit: true
-    },
+		github: {
+			dir: '.starters/default/content',
+			branch: 'master',
+			repo: 'nuxt-file-storage-docs',
+			owner: 'nyllre',
+			edit: true,
+		},
 
-    aside: {
-      level: 0,
-      collapsed: false,
-      exclude: []
-    },
+		aside: {
+			level: 0,
+			collapsed: false,
+			exclude: [],
+		},
 
-    main: {
-      padded: false,
-      fluid: true
-    },
+		main: {
+			padded: true,
+			fluid: true,
+		},
 
-    header: {
-      logo: true,
-      showLinkIcon: true,
-      exclude: [],
-      fluid: true,
-      title: 'Nuxt File Storage'
-    },
+		header: {
+			logo: true,
+			showLinkIcon: true,
+			exclude: [],
+			fluid: true,
+			title: 'Nuxt File Storage',
+		},
 
-    footer: {
-      credits: {
-        text: 'Created by Ali Sokkar with Docus'
-      }
-    },
+		footer: {
+			credits: {
+				text: 'Created by Ali Sokkar with Docus',
+			},
+		},
 
-    titleTemplate: '%s · Nuxt File Storage'
-  }
+		titleTemplate: '%s · Nuxt File Storage',
+	},
 })
